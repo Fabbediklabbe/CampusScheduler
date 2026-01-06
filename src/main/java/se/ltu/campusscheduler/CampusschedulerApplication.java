0421411d5a3432f6c5ac3ad473e6b3952750c3c5
Fabbediklabbe <blabbe.ny@gmail.com>
2026-01-06 12:54:48 +0100

    Spring Boot bas med Thymeleaf

---
+++ src/main/java/se/ltu/campusscheduler/CampusschedulerApplication.java
@@ -0,0 +1,13 @@
+package se.ltu.campusscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/ltu/campusscheduler/CampusschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ltu.campusscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
